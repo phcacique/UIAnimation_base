c141eb9b5dce2feec17339acfc251251d0a5ce44
felipe Petersen <felipekpetersen@gmail.com>
2019-05-27 17:50:59 -0300

    felipe animations

---
+++ UIAnimation/PRa.swift
@@ -0,0 +1,8 @@
+//
+//  PRa.swift
+//  
+//
+//  Created by Felipe Kaça Petersen on 27/05/19.
+//
+
+import Foundation
--- UIAnimation/ViewController.swift
+++ UIAnimation/ViewController.swift
@@ -14,10 +14,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
