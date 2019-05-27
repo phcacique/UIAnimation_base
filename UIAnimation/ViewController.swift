cf1d2358f1aedc1418b72e22a2b3b303837d6e9e
Edgar Sgroi <edgarsgroi@10-44-40-23.0101.mack.br.iosda.org>
2019-05-27 18:04:36 -0300

    Edgar First Commit

--- UIAnimation/ViewController.swift
+++ UIAnimation/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var world: UIImageView!
+    @IBOutlet weak var rocket: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 3.0, delay: 0, options: [.curveEaseOut], animations: {
+            self.rocket.center.y = self.world.center.y - self.rocket.frame.size.width * 2
+        }, completion: { finished in
+            self.rocket.image = UIImage(named: "rocket2.png")
+        })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.rocket.center.x = self.view.center.x
+//        self.rocket.frame.size.width = self.view.frame.size.height * 0.30
+        
+        self.world.frame.size.width = self.view.frame.size.width
+        self.world.center.x = self.view.center.x
+        self.world.center.y = self.view.frame.height
     }
 
 
